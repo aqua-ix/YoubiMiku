a67ef2db2f9cde438d51a3dee3c419be3fc2ddd9
Aqua-ix <souichi1997@gmail.com>
2018-12-04 01:19:55 +0900

    v2.5

--- app/src/main/java/comviewaquahp/google/sites/youbimiku/MainActivity.java
+++ app/src/main/java/comviewaquahp/google/sites/youbimiku/MainActivity.java
@@ -3,6 +3,7 @@ package comviewaquahp.google.sites.youbimiku;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import com.github.bassaer.chatmessageview.model.Message;
 import com.github.bassaer.chatmessageview.view.ChatView;
