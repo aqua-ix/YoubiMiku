484ba4994d8f962a863936aa077ac93ab362ddd7
Soichi Ikebe <souichi1997@gmail.com>
2021-08-02 00:28:11 +0900

    Update to v5.0

--- app/src/main/java/comviewaquahp/google/sites/youbimiku/MainActivity.kt
+++ app/src/main/java/comviewaquahp/google/sites/youbimiku/MainActivity.kt
@@ -1,36 +1,119 @@
 package comviewaquahp.google.sites.youbimiku
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.github.bassaer.chatmessageview.model.Message
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.google.android.gms.ads.admanager.AdManagerAdView
 import com.google.android.play.core.review.ReviewManagerFactory
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, DialogListener {
-    private lateinit var mUserAccount: User
-    private lateinit var mMikuAccount: User
+    private lateinit var userAccount: User
+    private lateinit var mikuAccount: User
 
     private lateinit var detectIntent: DetectIntent
+    private lateinit var adView: AdManagerAdView
+    private var initialLayoutComplete = false
+    private val scope = CoroutineScope(Dispatchers.Default)
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        detectIntent = DetectIntent(this)
 
-        // 起動回数カウント
+        initChatView(adSize.getHeightInPixels(this))
+        initBanner()
+        
+        showUserNameDialogIfNeeded()
+        showInAppReviewIfNeeded()
+    }
+
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = ad_view_container.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
+
+
+    private fun initBanner() {
+        MobileAds.initialize(this) {}
+        MobileAds.setRequestConfiguration(
+                RequestConfiguration.Builder()
+                        .setTestDeviceIds(listOf("emulator-5554"))
+                        .build()
+        )
+
+        ad_placeholder.layoutParams.height = adSize.getHeightInPixels(this)
+        adView = AdManagerAdView(this)
+        ad_view_container.addView(adView)
+        ad_view_container.viewTreeObserver.addOnGlobalLayoutListener {
+            if (!initialLayoutComplete) {
+                initialLayoutComplete = true
+                loadBanner(adSize)
+            }
+        }
+    }
+
+    private fun loadBanner(adSize: AdSize) {
+        adView.adUnitId = BuildConfig.AD_UNIT_ID
+        adView.setAdSizes(adSize)
+        val adRequest = AdManagerAdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
+
+    private fun initChatView(height: Int) {
+        val size = FontSizeConfig.getSize(getFontSizeType(this))
+        setFontSize(size, chat_view)
+
+        val mikuFace = BitmapFactory.decodeResource(resources, R.drawable.normal)
+        userAccount = User(0, null, null)
+        mikuAccount = User(1, getString(R.string.miku_name), mikuFace)
+        val mlp = chat_view.layoutParams as ViewGroup.MarginLayoutParams
+        mlp.topMargin = height
+        chat_view.setDateSeparatorFontSize(0F)
+        chat_view.setInputTextHint(getString(R.string.input_text_hint))
+        chat_view.setOnClickSendButtonListener(this)
+    }
+
+    private fun showGreet(userName: String?) {
+        val greeting = resources.getString(R.string.miku_nice_to_meet_you, userName)
+        val welcome = Message.Builder()
+                .setUser(mikuAccount)
+                .setRight(false)
+                .setText(greeting)
+                .build()
+        chat_view.receive(welcome)
+    }
+
+    private fun showInAppReviewIfNeeded() {
         val pref = SharedPreferenceManager
         pref.get(this, Key.LAUNCH_COUNT.name, 0)?.let {
             val current = it + 1
@@ -42,21 +125,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DialogListener {
                 pref.put(this, Key.LAUNCH_COUNT.name, 0)
             }
         }
+    }
 
-        detectIntent = DetectIntent(this)
-
-        initChatView()
+    private fun showUserNameDialogIfNeeded() {
         if (getUserName(this).equals("")) {
             showUserNameDialog(false)
         } else {
-            mUserAccount.setName(getUserName(this).toString())
+            userAccount.setName(getUserName(this).toString())
             showGreet(getUserName(this))
         }
-    }
-
-    public override fun onDestroy() {
-        super.onDestroy()
-        detectIntent.resetContexts()
     }
 
     private fun showUserNameDialog(cancelable: Boolean = true) {
@@ -69,7 +146,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DialogListener {
     }
 
     override fun doPositiveClick() {
-        mUserAccount.setName(getUserName(this).toString())
+        userAccount.setName(getUserName(this).toString())
         showGreet(getUserName(this))
     }
 
@@ -110,8 +187,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DialogListener {
                         .addOnSuccessListener {
                         }
             }
-        }
-        catch (e: Exception) {
+        } catch (e: Exception) {
         }
     }
 
@@ -129,7 +205,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DialogListener {
                 putExtra(Intent.EXTRA_TEXT, text)
             }
             startActivity(intent)
-        }catch (e: Exception){
+        } catch (e: Exception) {
             Toast.makeText(this,
                     getString(R.string.setting_send_feedback_error),
                     Toast.LENGTH_SHORT)
@@ -170,7 +246,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DialogListener {
             return
         }
         val send = Message.Builder()
-                .setUser(mUserAccount)
+                .setUser(userAccount)
                 .setRight(true)
                 .setText(chat_view.inputText)
                 .hideIcon(true)
@@ -180,71 +256,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DialogListener {
         chat_view.inputText = ""
     }
 
-    private fun onResult(response: String) {
-        runOnUiThread {
-            //Update view to bot says
-            val receivedMessage = Message.Builder()
-                    .setUser(mMikuAccount)
-                    .setRight(false)
-                    .setText(response)
-                    .build()
-            chat_view.receive(receivedMessage)
-        }
-    }
-
-    private fun onError(error: String?) {
-        runOnUiThread { Log.e(TAG, error.toString()) }
-    }
-
     private fun sendRequest(text: String) {
         Log.d(TAG, text)
         if (TextUtils.isEmpty(text)) {
-            onError(Constants.LOGGER_EMPTY_QUERY)
+            Log.e(TAG, Constants.LOGGER_EMPTY_QUERY)
             return
         }
-        AiTask().execute(text, null, null)
+        scope.launch {
+            dialogFlowTask(text)
+        }
     }
 
-    private fun initChatView() {
-        val size = FontSizeConfig.getSize(getFontSizeType(this))
-        setFontSize(size, chat_view)
-
-        val mikuFace = BitmapFactory.decodeResource(resources, R.drawable.normal)
-        mUserAccount = User(0, null, null)
-        mMikuAccount = User(1, getString(R.string.miku_name), mikuFace)
-        chat_view.setDateSeparatorFontSize(0F)
-        chat_view.setInputTextHint(getString(R.string.input_text_hint))
-        chat_view.setOnClickSendButtonListener(this)
-    }
-
-    private fun showGreet(userName: String?) {
-        val greeting = resources.getString(R.string.miku_nice_to_meet_you, userName)
-        val welcome = Message.Builder()
-                .setUser(mMikuAccount)
-                .setRight(false)
-                .setText(greeting)
-                .build()
-        chat_view.receive(welcome)
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    inner class AiTask : AsyncTask<String, Void?, String?>() {
-        var error = ""
-        override fun doInBackground(vararg params: String): String {
-            val query = params[0]
-            return try {
-                detectIntent.send(query)
-            } catch (e: Exception) {
-                error = e.toString()
-                ""
+    private suspend fun dialogFlowTask(text: String) {
+        try {
+            val response = detectIntent.send(text)
+            val receivedMessage = Message.Builder()
+                    .setUser(mikuAccount)
+                    .setRight(false)
+                    .setText(response)
+                    .build()
+            withContext(Dispatchers.Main) {
+                chat_view.receive(receivedMessage)
             }
+        } catch (e: Exception) {
+            Log.e(TAG, e.toString())
         }
+    }
 
-        override fun onPostExecute(response: String?) {
-            response?.let {
-                onResult(it)
-            } ?: onError(error)
-        }
+    public override fun onPause() {
+        adView.pause()
+        scope.coroutineContext.cancel()
+        super.onPause()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        adView.resume()
+    }
+
+    public override fun onDestroy() {
+        adView.destroy()
+        detectIntent.resetContexts()
+        super.onDestroy()
     }
 
     companion object {
