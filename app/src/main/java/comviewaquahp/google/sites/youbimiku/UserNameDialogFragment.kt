44bc2b350377ec89c3d79694d43dd383e8021237
Soichi Ikebe <souichi1997@gmail.com>
2022-10-08 01:56:03 +0900

    Update AGP version

--- app/src/main/java/comviewaquahp/google/sites/youbimiku/UserNameDialogFragment.kt
+++ app/src/main/java/comviewaquahp/google/sites/youbimiku/UserNameDialogFragment.kt
@@ -5,7 +5,6 @@ import android.app.DialogFragment
 import android.os.Bundle
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.user_name_dialog.view.*
 import java.util.EventListener
 
 interface DialogListener: EventListener {
