f66f6c4772d7c0d04f5346029abd99e19eb05fb5
Soichi Ikebe <souichi1997@gmail.com>
2024-03-13 03:56:37 +0900

    Migrate to Kotlin DSL
    https://developer.android.com/build/migrate-to-kotlin-dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-parcelize")
+    id("com.google.gms.google-services")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "comviewaquahp.google.sites.youbimiku"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 29
+        versionName = "7.9"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    packagingOptions {
+        resources.excludes.add("META-INF/INDEX.LIST")
+        resources.excludes.add("META-INF/DEPENDENCIES")
+    }
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            manifestPlaceholders["imobile_Testing"] = "true"
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            manifestPlaceholders["imobile_Testing"] = "false"
+        }
+    }
+    flavorDimensions("main")
+    productFlavors {
+        create("ads") {
+            dimension = "main"
+        }
+        create("noAds") {
+            dimension = "main"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures.viewBinding = true
+    namespace = "com.aqua_ix.youbimiku"
+    lintOptions {
+        isAbortOnError = false
+        isCheckReleaseBuilds = false
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0") {
+        exclude(group = "com.android.support", module = "support-annotations")
+    }
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.media:media:1.6.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("com.google.firebase:firebase-core:21.1.1")
+    implementation("com.google.cloud:google-cloud-dialogflow:0.105.0-alpha")
+    implementation("io.grpc:grpc-okhttp:1.32.2")
+    implementation("com.github.bassaer:chatmessageview:2.0.1")
+    implementation("com.google.android.play:core:1.10.3")
+    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.work:work-runtime-ktx:2.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    implementation(platform("com.aallam.openai:openai-client-bom:3.1.0"))
+    implementation("com.aallam.openai:openai-client")
+    implementation("io.ktor:ktor-client-okhttp")
+
+    implementation(platform("com.google.firebase:firebase-bom:31.2.2"))
+    implementation("com.google.firebase:firebase-config-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+    implementation("com.google.android.gms:play-services-ads-identifier:18.0.1")
+    implementation(files("libs/imobileSdkAds.jar"))
+}
+
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "secrets.defaults.properties"
+}
