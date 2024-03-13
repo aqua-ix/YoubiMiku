b9ecce07891308fd24b97aa6c8c84b9fc6c5eae0
Soichi Ikebe <souichi1997@gmail.com>
2024-03-13 22:00:54 +0900

    Target SDK 34 (#61)
    * Modify greeting
    
    * Remove unused properties
    
    * Migrate to Kotlin DSL
    
    https://developer.android.com/build/migrate-to-kotlin-dsl
    
    * Upgrade target sdk
    
    * Upgrade AGP to 8.3.0
    
    * Update plugins version
    
    * ﻿Update system prompt
    
    * Update java version

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
