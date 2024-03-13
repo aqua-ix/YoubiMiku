ab729b1b5e45a6216aaca560efe71a6ee3e11c4d
Soichi Ikebe <souichi1997@gmail.com>
2024-03-14 03:43:48 +0900

    Implement ad network config

--- app/src/main/java/com/aqua_ix/youbimiku/Constants.kt
+++ app/src/main/java/com/aqua_ix/youbimiku/Constants.kt
@@ -2,7 +2,7 @@ package com.aqua_ix.youbimiku
 
 class Constants {
     companion object {
-        const val ARGUMENT_CANCELABLE="cancelable"
+        const val ARGUMENT_CANCELABLE = "cancelable"
         const val OPENAI_MODEL = "gpt-3.5-turbo"
     }
 }
@@ -11,7 +11,15 @@ class RemoteConfigKey {
     companion object {
         const val MAX_USER_TEXT_LENGTH = "max_user_text_length"
         const val MAX_TOKENS = "max_tokens"
+        const val AD_NETWORK = "ad_network"
         const val AD_DISPLAY_REQUEST_TIMES = "ad_display_request_times"
         const val OPENAI_ENABLED = "openai_enabled"
+    }
+
+    class AdNetwork {
+        companion object {
+            const val IMOBILE = "imobile"
+            const val IRONSOURCE = "ironsource"
+        }
     }
 }
